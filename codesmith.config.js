54375a86b9d76b00bdba86704606d4a38b071db2
lwyj123 <liang.pearce@gmail.com>
2019-04-24 16:47:16 +0800

    feat: add functional component

--- codesmith.config.js
+++ codesmith.config.js
@@ -78,43 +78,28 @@ module.exports = function (codesmith) {
         ]
     });
 
-    // codesmith.setGenerator('pure-component-ts', {
-    //     description: 'Add a new pure component(ts)',
-    //     questions: [
-    //         {
-    //             type: 'input',
-    //             name: 'packageName',
-    //             message: 'What is your new package name?',
-    //             validate: function (value) {
-    //                 if ((/.+/).test(value)) { return true; }
-    //                 return 'name is required';
-    //             }
-    //         },
-    //         {
-    //             type: 'list',
-    //             name: 'platform',
-    //             message: 'What pizza toppings do you like?',
-    //             choices: [
-    //                 {name: 'None', value: 'none', checked: true},
-    //                 {name: 'Github', value: 'github'}
-    //             ]
-    //         },
-    //         {
-    //           type: 'input',
-    //           name: 'githubPath',
-    //           message: 'What is your Github repository path(like sundogrd/codesmith)?',
-    //           when: function(answers) {
-    //               return answers.platform === "github"
-    //           }
-    //         }
-    //     ],
-    //     actions: [
-    //         {
-    //             type: 'add',
-    //             path: 'package.json',
-    //             templateFile: 'templates/package.json',
-    //             abortOnFail: true
-    //         }
-    //     ]
-    // });
+    codesmith.setGenerator('functional-component-ts', {
+        description: 'Add a new functional component(ts)',
+        questions: [
+            {
+                type: 'directory',
+                name: 'basePath',
+                message: 'Where you like to put this component?',
+                basePath: ".",
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: "What's your component class name?"
+            }
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: '{{basePath}}/{{dashCase name}}.tsx',
+                templateFile: 'generators/functional-component-ts/templates/component.tsx',
+                abortOnFail: true
+            }
+        ]
+    });
 };
---
+++ generators/functional-component-ts/templates/component.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+function {{pascalCase name}}() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+    </div>
+  );
+}
